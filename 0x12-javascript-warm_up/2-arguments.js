880df596239941fca4e08a6550062c6531f5c12b
addisYonas <addyonas@gmail.com>
2022-10-04 08:56:20 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const Args = process.argv.length - 2;
-if (Args === 0) {
+const numArgs = process.argv.length - 2;
+if (numArgs === 0) {
   console.log('No argument');
-}else if (Args === 1){
-console.log('Arguments found')
- }else {
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
   console.log('Arguments found');
 }
